b109d18de134ef17989deea9b55d2ab68824b785
Kristoffer Magelssen <krislm15@uia.no>
2019-05-14 14:13:02 +0200

    Rewrote sketch_generator with a new example code from internet. Added example and result image.

--- pokemon/sketch_generator.py
+++ pokemon/sketch_generator.py
@@ -1,36 +1,26 @@
-import imageio
-import numpy as np
-import scipy.ndimage
-import matplotlib.pyplot as plt
+import cv2
 
-# Load image(s)
-img = ""
-start_img = imageio.imread(img)
+# reading the image
+img = cv2.imread('dataset/human/original/24729177_1.jpg', 1)
+# converting the image to grayscale
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# inverting the image
+img_invert = cv2.bitwise_not(img_gray)
+# blurring or smoothing the inverted image with the kernel size (10,10)
+img_blur = cv2.blur(img_invert, (10, 10))
 
-
-# Function for applying greyscaling to an image.
-def grayscale(rgb):
-    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
-
-
-gray_img = grayscale(start_img)  # Applying grescaling to image.
-inverted_img = 255-gray_img  # Inverting image
-# Blurring image. As sigma increases, the blur increases.
-blur_img = scipy.ndimage.filters.gaussian_filter(inverted_img, sigma=5)
+"""The Dodge blend function divides the bottom layer by the inverted top layer. 
+This lightens the bottom layer depending on the value of the top layer. 
+We have the blurred image, which highlights the boldest edges."""
 
 
-# The color dodge divides the bottom layer by the inverted top layer.
-# This lightens the bottom layer depending on the value of the top layer.
-# The blurred image highlights the boldest edges.
-def dodge(front, back):
-    result = front*255/(255-back)
-    result[result > 255] = 255
-    result[back == 255] = 255
-    return result.astype("uint8")
+def dodgeV2(image, mask):
+    return cv2.divide(image, 255 - mask, scale=256)
 
 
-final_img = dodge(blur_img, gray_img)
+final_img = dodgeV2(img_gray, img_blur)
 
-# Plotting and saving the image.
-plt.imshow(final_img, cmap="gray")
-plt.imsave("img2.png", final_img, cmap="gray", vmin=0, vmax=255)
+# displaying the sketch image
+cv2.imshow('24729177_1.jpg', final_img)
+cv2.imwrite('dataset/human/sketch/24729177_1.jpg', final_img)
+cv2.destroyAllWindows()
